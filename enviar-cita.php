93977d6e0b33d14d52a7a9eb053cce839fa6188a
Fernando <steelco1326@gmail.com>
2022-06-29 15:14:41 -0500

    comit

---
+++ enviar-cita.php
@@ -0,0 +1,44 @@
+<?php
+    $email = $_POST["email"];
+    $date = $_POST["date"];
+
+    $body = "Correo: " . $email . "<br>Fecha de cita: " . $date;
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/Exception.php';
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/SMTP.php';
+    $mail = new PHPMailer(true);
+
+    try{
+        //server settings
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host = 'avtrz.io';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'contact@avtrz.io';
+        $mail->Password = 'Cholula1234';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+
+        //recipients
+        $mail->setFrom($email,'Cita agendada');
+        $mail->addAddress('avtrz@hexabit.one');
+        //$mail->addAddress('diego1@avtrz.io');
+        //content
+        $mail->isHTML(true);
+        $mail->Subject = 'AVTRZ Contact';
+        $mail->Body = $body;
+
+        $mail->send();
+        echo '<script>
+                alert("El mensaje se envió correctamente");
+                window.history.go(-1);
+
+                </script>';
+    } catch (Exception $e){
+        echo 'Hubo un error al enviar el mensaje',$mail->ErrorInfo;
+    }
+?>
